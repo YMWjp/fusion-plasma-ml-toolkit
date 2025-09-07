6b13439113e7f84f04b105bbe9f8a23063384640
YUTA <030122yuta@gmail.com>
2025-09-08 01:34:48 +0900

    ファイル整理

--- data/makedata/get_params/get_beta_e.py
+++
@@ -1,5 +0,0 @@
-from neubeta import IotaGet
-
-def get_beta_e(self):
-        self.beta_e = IotaGet.get_betaiota(self.shotNO, self.time_list)[0]
-        return
--- data/makedata/get_params/get_fig.py
+++
@@ -1,8 +0,0 @@
-from classes.eg_read import eg_read
-
-
-def get_fig(self, shotNO):
-    eg = eg_read("fig_h2@" + str(self.shotNO) + ".dat")
-    self.fig6I = eg.eg_f1("FIG(6I_W)", self.time_list)
-    self.pcc3O = eg.eg_f1("Pcc(3-O)", self.time_list)
-    self.figpcc = self.fig6I / self.pcc3O
